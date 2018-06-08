f7a10a25f07488a0dd407bc4bc839d2c217685a6
zaki <yamawork1450@gmail.com>
2018-06-08 02:15:39 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,7 +3,7 @@ class ToppagesController < ApplicationController
     if logged_in?
       @user = current_user
       @micropost = current_user.microposts.build  # form_for 用
-      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
     end
   end
 end
